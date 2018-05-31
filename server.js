7078a337b64ac5149670110361f2e6bd8edba929
jhonnybenavidesm <jhonny1290bena@gmail.com>
2018-05-31 13:19:11 -0500

    thir4d2 commit

--- server.js
+++ server.js
@@ -12,57 +12,198 @@ var users = [
         user: 'admin',
         password: '123456',
         name: 'admin',
-        email: 'admin@movies.com',
+        email: 'admin@worldcup.com',
         img_user: 'https://www.littlemiracles.com.au/wp-content/uploads/2015/08/kid-on-ipad.png'
     }
 ];
 
-
-let movies = [
-    {id: 1, titulo: 'Avengers Infinity War', genero: 'Fantasia', anio: '2018', director: 'Anthony Russo',imagen:'http://www.rockandpop.cl/wp-content/uploads/2018/03/landscape-1500890190-avengers-infinity-war-poster-resized-1.jpg', trailer:'https://new.cinemark.com.co/movie?tag=2400&corporate_film_id=176879'},
-    {id: 2, titulo: 'Nada que Perder', genero: 'Accion/Aventura' , anio: '2018', director: 'Alexandre Avancini'},
-    {id: 3, titulo: 'El no nacido', genero: 'Terror', anio: '2009', director: 'David S. Goyer'},
-    {id: 5, titulo: 'Deadpool2', genero: 'Fantasia/Comedia', anio: '2018', director: 'David Leitch'},
-    {id: 6, titulo: 'Get Out', genero: 'Drama', anio: '2017', director: 'Jordan Peele'},
-    {id: 7, titulo: 'Logan', genero: 'Fantasia/Comedia', anio: '2017', director: 'James Mangold'},
-    {id: 8, titulo: 'Wonder Woman', genero: 'Fantasia', anio: '2017', director: 'Patty Jenkins'},
-    {id: 9, titulo: 'My Life as a Zucchini', genero: 'Fantasia', anio: '2016', director: 'Claude Barras'},
-    {id: 10, titulo: 'Am Not Your Negro ', genero: 'Documental', anio: '2017', director: 'Raoul Peck'},
-    {id: 11, titulo: 'Hidden Figures', genero: 'Drama', anio: '2016', director: 'Theodore Melfi'},
-    {id: 12, titulo: 'The Salesman (Forushande) ', genero: 'Drama', anio: '2017', director: 'Asghar Farhadi'},
-    {id: 13, titulo: 'Truman', genero: 'Drama/Comedia', anio: '1999', director: 'Peter Weir'},
-    {id: 14, titulo: 'The Lego Batman Movie ', genero: 'Animada', anio: '2017', director: 'Chris McKay'},
-    {id: 15, titulo: 'our Name. (Kimi No Na Wa.)', genero: 'Drama/Romantico', anio: '2016', director: 'Makoto Shinkai'},
-    {id: 16, titulo: 'Coco', genero: 'Familiar/Infantil', anio: '2017', director: 'Lee Unkrich'},
-    {id: 17, titulo: 'Star Wars', genero: 'Fantasia', anio: '2017', director: 'George Lucas'},
-    {id: 18, titulo: 'Home', genero: 'Familiar/Infantil', anio: '2015', director: 'Tim Johnson'},
-    {id: 19, titulo: 'sherlock holmes', genero: 'Aventura', anio: '2009', director: 'Guy Ritchie'},
-    {id: 20, titulo: 'mujer que no sabia leer', genero: 'Drama', anio: '2017', director: 'Marine Francen'},
-    {id: 21, titulo: 'Verdad o reto', genero: 'Suspenso', anio: '2018', director: 'Jeff Wadlow'},
-    {id: 22, titulo: 'Mi familia del norte', genero: 'Comedia', anio: '2018', director: 'Dany Boon'},
-    {id: 23, titulo: 'Operación huracán', genero: 'Accion', anio: '2018', director: 'Rob Cohen'},
-    {id: 24, titulo: 'Doctrina: el pecado original y la reinserción', genero: 'Terror', anio: '2018', director: 'Joan Frank'},
-    {id: 25, titulo: 'Lean on Pete', genero: 'Accion', anio: '2018', director: ' Andrew Haigh'},
-    {id: 26, titulo: 'El ataúd de cristal', genero: 'Terror', anio: '2016', director: 'Haritz Zubillaga '},
-
-
-   
+var pelicula = [
+    {
+        {id: 1,
+        titulo: 'Avengers Infinity War', 
+        genero: 'Fantasia', 
+        anio: '2018', 
+        director: 'Anthony Russo',
+        imagenn:'http://www.rockandpop.cl/wp-content/uploads/2018/03/landscape-1500890190-avengers-infinity-war-poster-resized-1.jpg',
+        trailer:'https://new.cinemark.com.co/movie?tag=2400&corporate_film_id=176879'},
+    },
+    {
+        id: 2, 
+        titulo: 'Nada que Perder', 
+        genero: 'Accion/Aventura' , 
+        anio: '2018', 
+        director: 'Alexandre Avancini'
+    }
+    
 ];
 
+var generos = [
+    {
+        genero: 'FANTASIA',
+        team1: 'Rusia',
+        team2: 'Arabia Saudí',
+        team3: 'Egipto',
+        team4: 'Uruguay',
+        team_img1: 'http://www.rockandpop.cl/wp-content/uploads/2018/03/landscape-1500890190-avengers-infinity-war-poster-resized-1.jpg',
+        team_img2: 'http://flags.fmcdn.net/data/flags/w1160/sa.png',
+        team_img3: 'http://flags.fmcdn.net/data/flags/w1160/eg.png',
+        team_img4: 'http://flags.fmcdn.net/data/flags/w1160/uy.png'
+    },
+    {
+        genero: 'GRUPO B',
+        team1: 'Portugal',
+        team2: 'España',
+        team3: 'Marruecos',
+        team4: 'RI de Irán',
+        team_img1: 'http://flags.fmcdn.net/data/flags/w1160/pt.png',
+        team_img2: 'http://flags.fmcdn.net/data/flags/w1160/es.png',
+        team_img3: 'http://flags.fmcdn.net/data/flags/w1160/ma.png',
+        team_img4: 'http://flags.fmcdn.net/data/flags/w1160/ir.png'
+    },
+    
+];
+
+var stadiums = [
+    {
+        id: '1',
+        name: 'Estadio Kaliningrado',
+        description: 'xxx',
+        star: '3',
+        url_img: 'https://img.fifa.com/image/upload/t_l4/zd4xfvozxlbz4ogzvl84.jpg'
+    },
+    {
+        id: '2',
+        name: 'Estadio Volgogrado Arena',
+        description: 'xxx',
+        star: '3',
+        url_img: 'https://img.fifa.com/image/upload/t_l4/n0i7ecb1dlfjkcevjn68.jpg'
+    },
+    {
+        id: '3',
+        name: 'Estadio Ekaterimburgo Arena',
+        description: 'xxx',
+        star: '3',
+        url_img: 'https://img.fifa.com/image/upload/t_l4/qwmfhwry65bhnowuu792.jpg'
+    },
+    {
+        id: '4',
+        name: 'Estadio Fisht',
+        description: 'xxx',
+        star: '3',
+        url_img: 'https://img.fifa.com/mm/photo/tournament/competition/02/89/93/84/2899384_full-lnd.jpg'
+    },
+    {
+        id: '5',
+        name: 'Estadio Kazán Arena',
+        description: 'xxx',
+        star: '3',
+        url_img: 'https://img.fifa.com/image/upload/t_l4/iyvktjjff6om7clsussx.jpg'
+    },
+    {
+        id: '6',
+        name: 'Estadio Nizhni Nóvgorod',
+        description: 'xxx',
+        star: '3',
+        url_img: 'https://img.fifa.com/image/upload/t_l4/iwokfedscpj4defmjxef.jpg'
+    },
+    {
+        id: '7',
+        name: 'Estadio Luzhniki',
+        description: 'xxx',
+        star: '3',
+        url_img: 'https://img.fifa.com/image/upload/t_l4/p1askyr6af4jekbzujmx.jpg'
+    },
+    {
+        id: '8',
+        name: 'Estadio Samara Arena',
+        description: 'xxx',
+        star: '3',
+        url_img: 'https://img.fifa.com/image/upload/t_l4/d0mymt1ubl2pypmu3gn3.jpg'
+    },
+    {
+        id: '9',
+        name: 'Estadio Rostov Arena',
+        description: 'xxx',
+        star: '3',
+        url_img: 'https://img.fifa.com/image/upload/t_l4/e95vbvut3zk2z2kjaqf0.jpg'
+    },
+    {
+        id: '10',
+        name: 'Estadio Spartak',
+        description: 'xxx',
+        star: '3',
+        url_img: 'https://img.fifa.com/image/upload/t_l4/ju6b9ulfzqdxsliopfef.jpg'
+    },
+    {
+        id: '11',
+        name: 'Estadio San Petersburgo',
+        description: 'xxx',
+        star: '3',
+        url_img: 'https://img.fifa.com/image/upload/t_l4/zvisds7bd2u4nzayjat8.jpg'
+    },
+    {
+        id: '12',
+        name: 'Estadio Mordovia Arena',
+        description: 'xxx',
+        star: '3',
+        url_img: 'https://img.fifa.com/image/upload/t_l4/pe6ww45wda53kaj226y0.jpg'
+    }
+];
+
+var news = [
+    {
+        id: '1',
+        title: 'Rusia da la bienvenida al mundo',
+        subtitle: 'Tras años de duro trabajo, el país anfitrión se prepara para recibir a todos los aficionados que vengan a Rusia y a los combinados en liza.',
+        url_news: 'https://img.fifa.com/image/upload/t_tc1/fncpsqsxjsujcgqryl8p.jpg'
+    },
+    {
+        id: '2',
+        title: 'Live It Up, la canción oficial de Rusia 2018',
+        subtitle: 'Descubre ahora la música que nos acompañará durante la Copa Mundial de la FIFA Rusia 2018. Interpretada por Nicky Jam, con Will Smith y Era Isterefi.',
+        url_news: 'https://img.fifa.com/image/upload/t_tc1/ax7h2zljgevmmv8povax.jpg'
+    },
+    {
+        id: '3',
+        title: '356.700 entradas asignadas en solo 24 horas',
+        subtitle: 'Dado el gran interés que despierta el torneo, la FIFA recuerda a los aficionados que FIFA.com/tickets es la única página web oficial para la compra de entradas.',
+        url_news: 'https://img.fifa.com/image/upload/t_tc1/vobrzd4d2veojlldjqim.jpg'
+    }
+];
+
+// ***************************************************************
+// ***************************************************************
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-
-
-// ********************************************************************
-// ********************************************************************
-
-// URL raiz de la api
-// http://127.0.0.1:5000
+app.use(bodyParser.urlencoded({ extended: true })); 
 app.get('/', (req, res) => {
     res.status(200).send("Welcome to API REST")
 })
 
+// ***************************************************************
+// ***************************************************************
+
+// Listar todos los partidos
+app.get('/pelicula', (req, res) => {
+    res.send(pelicula)
+})
+
+// Listar todos los grupos
+app.get('/genero', (req, res) => {
+    res.send(generos)
+})
+
+// Listar todas las noticias
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+
+// Listar todos los estadios
+app.get('/stadium', (req, res) => {
+    res.send(stadiums)
+})
+
+// Validar user and pass 
 app.post('/login', (req, res) => {
     let data = req.body;
     let login = [{searchUser: false,id: '0',user: '',password: '',name: '',email: '',img_user:''}];
@@ -85,14 +226,18 @@ app.post('/login', (req, res) => {
     res.send(login)
 })
 
-// ********************************************************************
-// ********************************************************************
-
-
+// Metodo para crear una cuenta de usuario
 app.post('/signup', (req, res) => {
     let data = req.body;
     let consecutive = users.length;
-  
+    // let itemUser = {
+    //     id: consecutive,
+    //     user: data.user,
+    //     password: data.pass,
+    //     name: data.name,
+    //     email: data.email,
+    //     img_user: 'https://www.littlemiracles.com.au/wp-content/uploads/2015/08/kid-on-ipad.png'
+    // };
     let itemUser = {
         user: data.user,
         password: data.pass,
@@ -105,51 +250,9 @@ app.post('/signup', (req, res) => {
     // res.send("usuario creado correctamente")
 })
 
-
-
-
-// URL para listar todos las peliculas
-// http://127.0.0.1:5000/users
-app.get('/movies', (req, res) => {
-    res.send(movies)
-})
-
-// URL para crear una pelicula
-// http://127.0.0.1:5000/users
-
-app.post('/movies', (req, res) => {
-   let data = req.query;
-    let items = {titulo: data.movies_name, genero: data.gen, anio: data.ani, director: data.dire}
-    movies.push(data.movies_name)
-    res.send("New movie add")
-})
-
-// URL para actualizar una pelicula
-// http://127.0.0.1:5000/users/1
-app.patch('/movies/:id',(req, res) => {
-    let params = req.params;
-    let data = req.query;
-    movies[params.id] = data.movies_name
-    res.send("Movie update")
-})
-
-
-
-// URL para eliminar una pelicula
-// http://127.0.0.1:5000/users/1
-app.delete('/movies/:id',(req, res) => {
-    let params = req.params;
-    movies.splice(params.id, 1);
-    res.send('Movies delete')
-})
-
-
-
-
-
-//**********************************************************************
-//**********************************************************************
-// Crear y lanzar el servidor
+// ***************************************************************
+// ***************************************************************
+ 
 http.createServer(app).listen(PORT, () => {
     console.log(`Server running at http://${hostname}:${PORT}/`);
 })
